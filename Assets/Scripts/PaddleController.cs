86ad95ffef35601275605aba0275a4a3ed994ed9
nozomigithub <nozomi.m0828@gmail.com>
2022-06-04 15:27:15 +0900

    lesson3sitayo

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -11,17 +11,25 @@ public class PaddleController : MonoBehaviour
     /// <summary>パドルの移動速度</summary>
     [SerializeField] float m_speed = 5f;
     Rigidbody2D m_rb2d;
+    
+
 
     void Start()
     {
-        m_rb2d = GetComponent<Rigidbody2D>();
+       m_rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
+    {
+        m_rb2d.
+        Move();
+    }
+    void Move()
     {
         // 水平方向の入力を検出する
         float h = Input.GetAxisRaw("Horizontal");
         // 入力に応じてパドルを水平方向に動かす
         m_rb2d.velocity = h * Vector2.right * m_speed;
+        
     }
 }
